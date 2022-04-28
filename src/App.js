fce36723ada11a1f61ff359a7a6890cce657a4d6
Mario Rivera <mario.rivera@xideral.co>
2022-04-28 14:16:32 -0500

    Se acomoda la barra de herramientas

--- src/App.js
+++ src/App.js
@@ -1,74 +1,90 @@
 import * as React from "react";
-
-import { DataGrid, GridRowsProp, GridColDef, GridToolbarContainer,
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import {
+  DataGrid,
+  esES,
+  GridRowsProp,
+  GridColDef,
+  GridToolbarContainer,
   GridToolbarColumnsButton,
   GridToolbarFilterButton,
   GridToolbarExport,
-  GridToolbarDensitySelector, } from "@mui/x-data-grid";
-  import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+  GridToolbarDensitySelector,
+} from "@mui/x-data-grid";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 
-  const columns = [
-    {
-      field: 'first',
-      headerName: 'First',
-      width: 140,
-    },
-    {
-      field: 'last',
-      headerName: 'Last',
-      width: 140,
-    },
-  ];
-  
-  const initialRows = [
-    {
-      id: 1,
-      first: 'Jane',
-      last: 'Carter',
-    },
-    {
-      id: 2,
-      first: 'Jack',
-      last: 'Smith',
-    },
-    {
-      id: 3,
-      first: 'Gill',
-      last: 'Martin',
-    },
-  ];
+const columns = [
+  {
+    field: "first",
+    headerName: "First",
+    width: 140,
+  },
+  {
+    field: "last",
+    headerName: "Last",
+    width: 140,
+  },
+];
+
+const initialRows = [
+  {
+    id: 1,
+    first: "Jane",
+    last: "Carter",
+  },
+  {
+    id: 2,
+    first: "Jack",
+    last: "Smith",
+  },
+  {
+    id: 3,
+    first: "Gill",
+    last: "Martin",
+  },
+];
+
 function CustomToolbar() {
   return (
-    <GridToolbarContainer>
-
-      <GridToolbarColumnsButton startIcon={<AccessAlarmIcon />} />
-      
-      <p>Paises</p>
-      {/* <GridToolbarDensitySelector /> */}
-      <GridToolbarFilterButton />
-      <GridToolbarExport />
-    </GridToolbarContainer>
+    <Box sx={{ flexGrow: 1 }}>
+      <GridToolbarContainer>
+        <Grid container>
+          <Grid item xs={3}>
+            <GridToolbarColumnsButton startIcon={<AccessAlarmIcon />} />
+          </Grid>
+          <Grid item xs={6}>
+            <p>Paises</p>
+          </Grid>
+          <Grid item xs={1}>
+            <GridToolbarFilterButton />
+          </Grid>
+          <Grid item xs={1}>
+            <GridToolbarExport />
+          </Grid>
+          <Grid item xs={1}>
+            <GridToolbarExport />
+          </Grid>
+        </Grid>
+      </GridToolbarContainer>
+    </Box>
   );
 }
+
 function App() {
-  
   return (
     <div style={{ height: 300, width: "100%" }}>
-      <DataGrid 
-        rows={initialRows} 
-        columns={columns} 
+      <DataGrid
+        localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+        rows={initialRows}
+        columns={columns}
         disableColumnMenu={true}
+        hideFooter={true}
         hideFooterPagination={true}
-        localeText={{
-          toolbarFilter: 'Size',
-          toolbarFilterLabel: 'Size',
-          toolbarFilterCompact: 'Large',
-          toolbarFilterStandard: 'Large',
-          toolbarFilterComfortable: 'Large',
-        }}
         components={{
           Toolbar: CustomToolbar,
-        }} />
+        }}
+      />
     </div>
   );
 }
