b8463bdbaf236d2c9c01834e0d131e2c647e9882
Mario Rivera <mario.rivera@xideral.co>
2022-04-29 08:31:44 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import {useState} from "react";
+import { useState } from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import { styled, alpha } from '@mui/material/styles';
-import SearchIcon from '@mui/icons-material/Search';
-import InputBase from '@mui/material/InputBase';
+import { styled, alpha } from "@mui/material/styles";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
 import {
   DataGrid,
   esES,
@@ -14,6 +13,11 @@ import {
   GridToolbarExport,
 } from "@mui/x-data-grid";
 import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import FilterAltRoundedIcon from "@mui/icons-material/FilterAltRounded";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import FileDownloadRoundedIcon from "@mui/icons-material/FileDownloadRounded";
+import { Button } from "@mui/material";
+import "./styles.css";
 
 const columns = [
   {
@@ -66,87 +70,91 @@ const initialRows = [
     action: "Edit",
   },
 ];
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(3),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "100%",
     },
   },
 }));
 function CustomToolbar() {
-  const [activeSearch, setActiveSearch] = useState(false)
+  const [activeSearch, setActiveSearch] = useState(false);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <GridToolbarContainer sx={{diplay: 'flex', flexDirection: 'column'}}>
+      <GridToolbarContainer sx={{ diplay: "flex", flexDirection: "column" }}>
         <Grid container>
           <Grid item xs={3}>
             <GridToolbarColumnsButton
-              sx={{ fontSize: 0 }}
-              startIcon={<AccessAlarmIcon sx={{ color: "red" }} />}
+              className="noLabel"
+              startIcon={<MenuRoundedIcon className="iconToolbar rotateIcon" />}
             />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={6} sx={{ textAlign: "center", fontWeight: "bold" }}>
             <p>Paises</p>
           </Grid>
-          <Grid item xs={1}>
+          <Grid item xs={3}>
+            <Button>
+              <SearchIcon
+                className="iconToolbar"
+                onClick={() => setActiveSearch(!activeSearch)}
+              />
+            </Button>
             <GridToolbarFilterButton
-              sx={{ fontSize: 0 }}
-              startIcon={<AccessAlarmIcon sx={{ color: "red" }} />}
+              className="noLabel"
+              startIcon={<FilterAltRoundedIcon className="iconToolbar" />}
             />
-          </Grid>
-          <Grid item xs={1}>
             <GridToolbarExport
-              sx={{ fontSize: 0 }}
-              startIcon={<AccessAlarmIcon sx={{ color: "red" }} />}
+              className="noLabel"
+              startIcon={<FileDownloadRoundedIcon className="iconToolbar" />}
             />
-          </Grid>
-          <Grid item xs={1}>
-            <SearchIcon sx={{color: 'red'}} onClick={() => setActiveSearch(!activeSearch)} />
           </Grid>
         </Grid>
-        {activeSearch && <Grid container>
-          <Search>
+        {activeSearch && (
+          <Grid container>
+            <Search>
               <SearchIconWrapper>
                 <SearchIcon />
               </SearchIconWrapper>
               <StyledInputBase
                 placeholder="Search…"
-                inputProps={{ 'aria-label': 'search' }}
+                inputProps={{ "aria-label": "search" }}
               />
-          </Search>
-        </Grid> }
+            </Search>
+          </Grid>
+        )}
       </GridToolbarContainer>
     </Box>
   );
